d6f18371fda6f5665472093151275d6f97c11e6d
Adrian Arce <arce.diaz.adrian@gmail.com>
2023-05-28 21:21:55 -0300

    Added list for permissions as well

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import PermissionsList from "./pages/permissions/PermissionsList";
 import PermissionTypesList from "./pages/permissionTypes/PermissionTypesList";
 
 function App() {
+  const [permissions, setPermissions] = useState([]);
   const [permissionTypes, setPermissionTypes] = useState([]);
 
   const getInitialData = async () => {
+    const permissionsResult = await axios.get(
+      `${process.env.REACT_APP_PERMISSIONS_API_URL}/Permissions`
+    );
     const permissionTypesResult = await axios.get(
       `${process.env.REACT_APP_PERMISSIONS_API_URL}/PermissionTypes`
     );
 
+    setPermissions(permissionsResult.data);
     setPermissionTypes(permissionTypesResult.data);
   };
 
@@ -23,9 +29,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route
+          path="/permissions"
+          element={<PermissionsList permissions={permissions} />}
+        />
+        <Route
           path="/permissionTypes"
           element={<PermissionTypesList permissionTypes={permissionTypes} />}
         />
+        <Route path="*" element={<Navigate to="/permissions" replace />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ web/src/pages/permissions/PermissionsList.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+import { Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import EditIcon from "@mui/icons-material/Edit";
+import IconButton from "@mui/material/IconButton";
+
+import { useNavigate } from "react-router-dom";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(3),
+  color: theme.palette.text.secondary,
+  elevation: 3,
+  margin: "20px",
+}));
+
+function PermissionsList({ permissions }) {
+  const navigate = useNavigate();
+
+  const navigateToEdit = (id) => {
+    navigate(`/permissions/${id}`);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "stretch",
+      }}
+    >
+      <Box sx={{ width: "60vw" }}>
+        {permissions.map((permission, index) => (
+          <Item key={index}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box sx={{ flexGrow: 6 }}>
+                <Typography sx={{ fontSize: 32 }} color="text.primary">
+                  {`${permission.employeeFirstName} ${permission.employeeLastName}`}
+                </Typography>
+                <Typography variant="h5">
+                  {permission.permissionType.description}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  {permission.grantedDate}
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "flex-end",
+                  marginRight: "20px",
+                }}
+              >
+                <IconButton
+                  size="large"
+                  onClick={() => navigateToEdit(permission.id)}
+                >
+                  <EditIcon />
+                </IconButton>
+              </Box>
+            </Box>
+          </Item>
+        ))}
+      </Box>
+    </Box>
+  );
+}
+
+export default PermissionsList;
